fef4c826c51285e5773823bf4e812808657b2cc3
zmiyov <zmiyov@i.ua>
2018-10-09 16:48:12 +0300

    post class + ancestors

---
+++ post.rb
@@ -0,0 +1,37 @@
+class post
+
+  def initilize
+    @create_at = Time.now
+    @text = nil
+  end
+
+
+  def read_from_console
+    # todo
+  end
+
+
+  def to_strings
+    # todo
+  end
+
+
+  def save
+    file = File.new(file_path, "w:UTF-8")
+
+    for item in to_strings do
+      file.puts(item)
+    end
+
+    file.close
+
+  end
+
+  def file_puth
+    current_path = File.dirname (__FILE__)
+
+    file_name = @create_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
+
+    return current_path + "/" + file_name
+  end
+end
---
+++ task.rb
@@ -0,0 +1,14 @@
+class Task < Post
+  def initialize
+    super
+
+    @due_date = Time.now
+
+    def read_from_console
+    end
+
+    def to_strings
+    end
+
+
+  end
